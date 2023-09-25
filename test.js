3d771050c59416214ed7a3ee635b57ac794775eb
wanghoi <wanghoi@126.com>
2023-09-25 17:53:43 +0800

    构建小修改

--- test.js
+++ test.js
@@ -1,14 +1,23 @@
 "use strict";
 "use math";
 
-
 function JSX(tag,atts,kids) {
 	return [tag,atts,kids];
 }
 
+class Div extends Object {
+	render(props, kids) {
+		return <div>
+			<p>{props.a}</p>
+			{kids}
+			<p>next kids</p>
+		</div>
+	}
+};
+
 function main() {
-	var a = <div id="aa">Hello world!</div>;
-	console.log(JSON.stringify(a));
+	var a = <Div a="attr1"><p>aa<Div>jjj</Div></p></Div>;
+	console.log(`${JSON.stringify(a)}`);
 }
 
 main();
