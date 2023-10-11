a294cb6624b3f75d17858af8cbc4fba68c3d1d76
wanghoi <wanghoi@126.com>
2023-10-11 09:11:13 +0800

    增加测试文件

---
+++ mock.js
@@ -0,0 +1,79 @@
+"use strict";
+"use math";
+
+class __ComponentState__ extends Object {
+	renderFn;
+	props;
+	children;
+
+	constructor(builder, props, children) {
+		super();
+		this.renderFn = builder;
+		this.props = props;
+		this.children = children;
+	}
+	update() {
+		// this.scene.updateComponent(this);
+	}
+	render() {
+		return this.renderFn(this.props, this.children);
+	}
+};
+
+
+function JSX(tag, atts, kids) {
+	if (typeof tag == 'function') {
+		return new __ComponentState__(tag, atts, kids);
+	} else {
+		return {
+			tag,
+			atts,
+			kids,
+		};
+	}
+}
+
+function Div(props, kids) {
+	return <div>
+		<p>{props.a}</p>
+		{kids}
+		<p>next</p>
+	</div>;
+}
+
+var simple_stylesheet = {
+	".title-bar": {
+		"position": "absolute",
+		"top": 0,
+		"right": 0,
+		"margin-left": 10,
+		"margin-top": 10,
+		"margin-right": 10,
+		"margin-bottom": 10
+	}
+};
+
+var hello_world = (<body>
+    <div>橙讯安装程序</div>
+    <div class="title-bar">
+        <button>最小化</button>
+        <button>最大化</button>
+        <button>关闭</button>
+    </div>
+    <div style={{margin-top: 40, text-align: "center"}}>
+        <img src="cx_logo_2.svg" />
+    </div>
+    <div style={{margin-top: 40, text-align: "center"}}>
+        <span style={{position:"relative", font-size: 20}}>
+            橙讯
+            <span style={{position:"absolute", left:"100%", top:-10, font-size:10}}">2.7.0</span>
+        </span>
+    </div>
+    <div style={{text-align:"center"}}>
+        <button style={{margin-top: 40}}>安装</button>
+    </div>
+</body>);
+
+console.log(JSON.stringify(hello_world));
+
+app.showDialog(hello_world, simple_stylesheet);
