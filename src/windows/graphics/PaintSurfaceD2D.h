27e7e3be444c6dc3cf002cccc50231acd77db4df
wanghoi <wanghoi@126.com>
2024-04-03 17:39:57 +0800

    add paint surface d2d

--- src/windows/graphics/PaintSurfaceD2D.h
+++ src/windows/graphics/PaintSurfaceD2D.h
@@ -5,8 +5,11 @@ namespace windows
 namespace graphics
 {
 
-class PainterSurfaceD2D : public graph2d::PaintSurfaceInterface {
-
+class PaintSurfaceD2D : public graph2d::PaintSurfaceInterface {
+public:
+    void resize(int pixel_width, int pixel_height, float dpi_scale) override;
+    std::unique_ptr<graph2d::PainterInterface> beginPaint() override;
+    bool endPaint() override;
 };
 
 } // namespace graphics
