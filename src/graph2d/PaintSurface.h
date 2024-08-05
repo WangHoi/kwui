9203cec2d04d883f0bbfc094b1f49351033fae64
wanghoi <wanghoi@126.com>
2024-08-05 10:21:05 +0800

    fix PaintSurface leak

--- src/graph2d/PaintSurface.h
+++ src/graph2d/PaintSurface.h
@@ -6,6 +6,7 @@ namespace graph2d
 class PainterInterface;
 class PaintSurfaceInterface {
 public:
+    virtual ~PaintSurfaceInterface() = default;
     virtual void resize(int pixel_width, int pixel_height, float dpi_scale) = 0;
     virtual std::unique_ptr<PainterInterface> beginPaint() = 0;
     virtual bool endPaint() = 0;
